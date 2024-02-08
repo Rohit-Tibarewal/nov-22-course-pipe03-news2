178f3b1fc388f3010e98d609fe0a4e1b89f606f9
Rohit Tibarewal <Rohit.Tibarewal@softwareag.com>
2024-02-08 19:01:48 +0530

    updated acr repo name

--- buildScripts/setEnv.sh
+++ buildScripts/setEnv.sh
@@ -6,5 +6,5 @@ export AZ_BASE_IMAGE_REPO="msr-1015-lean-original-recipe"
 export AZ_BASE_IMAGE_FIXES_TAG="Fixes_24-01-16"
 export AZ_BASE_IMAGE_TAG="${MY_AZ_ACR_URL}/${AZ_BASE_IMAGE_REPO}:${AZ_BASE_IMAGE_FIXES_TAG}"
 
-export AZ_ACR_REPO_NAME="my-news-service-01"
+export AZ_ACR_REPO_NAME="my-news-service-02"
 export OUR_SERVICE_TAG_BASE="${MY_AZ_ACR_URL}/${AZ_ACR_REPO_NAME}"
